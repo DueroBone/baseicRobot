6111b1b44ad31d55c2bfa9d56db2ab26cfa28f00
SanguineSlayer6 <blackhexfury896@protonmail.com>
2024-01-10 16:51:39 -0500

    magic

--- src/main/java/frc/robot/commands/TeleDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleDriveCommand.java
@@ -1,9 +1,9 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 
-public class TeleDriveCommand extends CommandBase {
+public class TeleDriveCommand extends Command {
   public TeleDriveCommand() {
     addRequirements(Robot.DriveTrain);
   }
