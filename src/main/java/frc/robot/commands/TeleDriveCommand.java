08877e75b05c272889d45027761adf6408e05415
Duel M <dueromoods@gmail.com>
2024-01-11 12:48:21 -0500

    Reduced import verbosity

--- src/main/java/frc/robot/commands/TeleDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleDriveCommand.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
-import frc.robot.Robot.*;
 
 public class TeleDriveCommand extends Command {
   public TeleDriveCommand() {
